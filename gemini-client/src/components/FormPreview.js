3eabad25c68ab4da908afb16b3fd54477b91ea0c
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-10-02 19:10:30 -0400

    added some styles

--- gemini-client/src/components/FormPreview.js
+++ gemini-client/src/components/FormPreview.js
@@ -149,15 +149,16 @@ const DisplayField = (props) => {
             </label>)
         case 'input':
             return (<label className="flex items-center space-x-3">
-                <span>{props.label}</span> <input />
+                <span>{props.label}</span> <input className="focus:bg-gray-800 px-2 py-1 focus:text-white rounded-lg " />
             </label>)
         case 'multiple choice':
             return (
                 <label className="flex items-center space-x-3">
                     <span>{props.label}</span>
-                    {props.options.length === 0 ? <div>no options</div> : <select>
-                        {props.options.map((o) => <option key={o.id} value={o.name}>{o.name}</option>)}
-                    </select>}
+                    {props.options.length === 0 ? <div>no options</div> :
+                        <select className="rounded-lg px-2 py-1">
+                            {props.options.map((o) => <option key={o.id} value={o.name}>{o.name}</option>)}
+                        </select>}
 
                 </label>
             )
