10cbccbb541078da52f9c98d12ae6f9da472b77c
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-10-02 16:13:06 -0400

    set up router

--- gemini-client/src/App.js
+++ gemini-client/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import InputForm from './components/InputForm'
-import ListForms from './components/ListForms';
+import ListForms from './components/ListForms'
+import FormPreview from './components/FormPreview'
+
+import {
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center space-y-10">
-      <InputForm />
-      <ListForms />
+      <Switch>
+        <Route exact path='/'><>
+          <InputForm />
+          <ListForms /></>
+        </Route>
+        <Route path="/forms-preview/:id">
+          <FormPreview />
+        </Route>
+      </Switch>
     </div>
   );
 }
