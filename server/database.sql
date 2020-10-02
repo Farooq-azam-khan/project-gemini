024df9ba9114e02581ad4286bf6948af6b1b48cf
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-10-02 16:13:31 -0400

    Update database.sql

--- server/database.sql
+++ server/database.sql
@@ -6,3 +6,16 @@ CREATE TABLE form (
     organizer VARCHAR(255), 
     is_published BOOLEAN 
 );
+
+CREATE TABLE form_field (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255), 
+  label VARCHAR(255), 
+  form INT REFERENCES form(id)
+);
+
+CREATE TABLE options (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255), 
+  form_field INT REFERENCES form_field(id)
+);
