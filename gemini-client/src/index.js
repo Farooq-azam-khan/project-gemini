22289cb0ddfabcc09adbad4d067ae5dd1531b09d
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-10-03 16:21:43 -0400

    deployment code

--- gemini-client/src/index.js
+++ gemini-client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './css/output.css'
+// import './css/output.css'
+import './css/prod.css'
 import {
   BrowserRouter as Router,
 
